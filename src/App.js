c340bea09b36d98c04cd94b60a7cb7124fb3e40e
Parvathy <parvathyreghunath.mec@gmail.com>
2022-01-28 22:30:47 +0530

    inserted to do form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <div id='box'>
-        <div class="rectanglebox"></div>
+class App extends React.Component{
+  render(){
+    return(
+      <div className='App'>
+        <header>
+        <form id="to-do-form">
+          <input type="text" placeholder='Enter text'/>
+          <button id="to-do-form button"type='submit'>Add</button>
+        </form>
+      </header>
       </div>
-      
-        
-      
-    </div>
-  );
+    );
+  }
 }
+
+
+
+
+
 
 export default App;
